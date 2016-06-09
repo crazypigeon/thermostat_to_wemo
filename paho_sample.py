0df6a00d983f7fd6bedbedca401118ebb2d57bfa
Erik Granlund <erik@erikgranlund.com>
2016-06-08 22:57:37 -0700

    Got state matching working with wemo switches

--- paho_sample.py
+++ paho_sample.py
@@ -1,4 +1,21 @@
 import paho.mqtt.client as mqtt
+import pywemo
+import os
+
+# WeMo initial setup
+WEMO_SERIAL_FAN = os.environ['FAN_SERIAL_NUMBER']
+WEMO_SERIAL_COOL = os.environ['COOL_SERIAL_NUMBER']
+WEMO_FAN = None
+WEMO_COOL = None
+
+# TODO: Make it specify based on IP rather than serial number
+for device in pywemo.discover_devices():
+    print "Found WeMo: " + device.serialnumber
+    if device.serialnumber == WEMO_SERIAL_FAN:
+      WEMO_FAN = device
+    elif device.serialnumber == WEMO_SERIAL_COOL:
+      WEMO_COOL = device
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -6,13 +23,8 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("air_conditioner/status")
     client.subscribe("air_conditioner/set")
-
-    client.subscribe("fan/status")
     client.subscribe("fan/set")
-
-    client.subscribe("ignore_thermostat/status")
     client.subscribe("ignore_thermostat/set")
 
 # The callback for when a PUBLISH message is received from the server.
@@ -25,6 +37,18 @@ def on_message(client, userdata, msg):
       print('setting ' + topic_to_update + ' to ' + msg.payload)
 
       # do stuff
+      # TODO: Make more generic
+      device=None
+
+      if 'air_conditioner' in msg.topic:
+        device = WEMO_COOL
+      elif 'fan' in msg.topic:
+        device = WEMO_FAN
+
+      if int(msg.payload):
+        device.on()
+      else:
+        device.off()
 
       client.publish(topic_to_update,payload=msg.payload,retain=True)
 
@@ -38,4 +62,6 @@ client.connect("192.168.1.200")
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
+# client.loop_forever()
+while True:
+    client.loop()
